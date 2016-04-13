4a4d5f2050be26895da410b566b176164dea923f
alielabridi <elabridiali@outlook.com>
2016-04-12 23:25:57 -0700

    added adapter for listview

---
+++ app/src/main/java/com/example/aidan/mathfunfacts/CustomAdapter.java
@@ -0,0 +1,52 @@
+package com.example.aidan.mathfunfacts;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.example.aidan.mathfunfacts.MainActivity.collection;
+/**
+ * Created by Ali on 4/12/2016.
+ */
+
+public class CustomAdapter extends ArrayAdapter<ParserMathFunFact> {
+    public CustomAdapter(Context context, ArrayList<ParserMathFunFact> MathFunFacts) {
+        super(context, R.layout.custom_row, MathFunFacts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater ListInflater = LayoutInflater.from(getContext());
+        View customView = ListInflater.inflate(R.layout.custom_row, parent, false);
+
+        String title = getItem(position).getTitle();
+        int level = getItem(position).getLevel();
+        TextView titleListElement = (TextView) customView.findViewById(R.id.titleMFF);
+        titleListElement.setText(title);
+
+        //set the color depending on the difficulty
+        if(level == 1)[
+        //easy
+        titleListElement.setBackgroundColor(Color.YELLOW);
+        ]
+        else if (level == 2)[
+        //medium
+        titleListElement.setBackgroundColor(Color.BLUE);
+        ]
+        else[
+        // hard
+        titleListElement.setBackgroundColor(Color.BLACK);
+
+        ]
+
+        return customView;
+    }
+}
